0085e11c95bb2ff41cf29187762314622512d3b6
samuel-cs4 <samuel@4intelligence.com.br>
2022-02-01 16:03:39 -0300

    feat: validate values

--- playlist/script.js
+++ playlist/script.js
@@ -2,10 +2,18 @@
  * Add a song on screen
  */
 function addSong() {
+
+  // Output: A list of songs and artists
   var songList = window.document.getElementById('song-list')
 
-  var song = window.document.getElementById('input-song').value
-  var artist = window.document.getElementById('input-artist').value
+  // Input: Song and Artist
+  var song    = window.document.getElementById('input-song')?.value    ?? ''
+  var artist  = window.document.getElementById('input-artist')?.value  ?? ''
+
+  if(song === '' || artist === ''){
+    window.alert('Some values are empty! Please entry with a value on song and artist inputs.')
+    return null
+  }
 
   songList.innerHTML += `<p>${song} - ${artist}</p>`
   
