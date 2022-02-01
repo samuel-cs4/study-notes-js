94f1042c9814276bd3375a3f5b3aec65542468e9
samuel-cs4 <samuel@4intelligence.com.br>
2022-02-01 14:14:30 -0300

    feat: create script to display a song and artist

---
+++ playlist/script.js
@@ -0,0 +1,11 @@
+
+
+function addSong() {
+  var songList = window.document.getElementById('song-list')
+
+  var song = window.document.getElementById('input-song').value
+  var artist = window.document.getElementById('input-artist').value
+  
+  songList.innerHTML += `<p>${song} - ${artist}</p>`
+  
+}
