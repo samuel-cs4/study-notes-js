ff36f3049de46b708b435e918a396a169bcdf3a0
samuel-cs4 <samuel@4intelligence.com.br>
2022-02-01 15:56:16 -0300

    style: styling components

--- playlist/script.js
+++ playlist/script.js
@@ -1,11 +1,12 @@
-
-
+/**
+ * Add a song on screen
+ */
 function addSong() {
   var songList = window.document.getElementById('song-list')
 
   var song = window.document.getElementById('input-song').value
   var artist = window.document.getElementById('input-artist').value
-  
+
   songList.innerHTML += `<p>${song} - ${artist}</p>`
   
 }
